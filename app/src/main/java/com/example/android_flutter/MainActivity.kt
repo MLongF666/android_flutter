34096d7e8744e88279ee07f8bed0f97ef3d5c50b
马龙飞 <86140055+malongfei666@users.noreply.github.com>
2024-08-05 17:43:11 +0800

    Create README.md

--- app/src/main/java/com/example/android_flutter/MainActivity.kt
+++ app/src/main/java/com/example/android_flutter/MainActivity.kt
@@ -1,6 +1,26 @@
 package com.example.android_flutter
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity() {
+class MainActivity: AppCompatActivity(){
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btn = findViewById<Button>(R.id.btn)
+        btn.setOnClickListener {
+            Toast.makeText(this, "Flutter Activity", Toast.LENGTH_SHORT).show()
+            val intent = FlutterActivity.withNewEngine().initialRoute("android 启动 flutter")
+                .build(this)
+            startActivity(intent)
+
+
+        }
+    }
 
 }
